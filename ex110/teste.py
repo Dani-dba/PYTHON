d67da885b29aab537251c594dec3f7637389fb59
Danielle <danielle.goes_dba@gmail.com>
2024-10-01 12:11:48 -0300

    Incluindo exercicios recentes

--- ex110/teste.py
+++ ex110/teste.py
@@ -1,8 +1,4 @@
 import moeda
 preço = float(input('Digite o preço R$: '))
-print(f'A metade do preço {moeda.moeda(preço)} é igual a {moeda.metade(preço, True)}')
-print(f'O dobro do preço {moeda.moeda(preço)} é igual a {moeda.dobro(preço, True)}')
-print(f'Com 10% de juros o valor será {moeda.aumentar(preço, 10, True)}')
-print(f'Com 10% de desconto o valor será {moeda.diminuir(preço, 10, True)}')
-print(f'Com 13% de desconto o valor será {moeda.diminuir(preço, 13, True)}')
 
+moeda.resumo(preço)
---
+++ ex111/teste.py
@@ -0,0 +1,7 @@
+from pyton_6.pythonProject1.ex111.utilidadescev import moeda
+
+
+
+preço = float(input('Digite o preço R$: '))
+
+moeda.resumo(preço,15, 10)
